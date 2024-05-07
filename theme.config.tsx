a10c3419f757013c7a2e0ee29c6b531c5181ea66
saranshisatgit <saranshisatgit@protonmail.com>
2024-05-07 13:59:46 +0530

    Title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Kind Health </span>,
   project: {
     link: "https://github.com/Kind-Gestures-Tech/protocol",
   },
